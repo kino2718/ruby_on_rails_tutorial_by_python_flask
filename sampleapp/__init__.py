6f87e5b34f8459efbf14aaeab2a8180cf8afeb38
KINOMOTO, Masakazu <kino2718@gmail.com>
2020-12-01 14:11:34 +0900

    app.configにSECRET_KEYを設定。

--- sampleapp/__init__.py
+++ sampleapp/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import logging
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)

84167a1a24ce24501f9c7a926ca5a848378b7037
KINOMOTO, Masakazu <kino2718@gmail.com>
2020-12-01 10:48:40 +0900

    アプリをパッケージとして構成。

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,11 @@ def create_app(test_config=None):
 
     @app.route('/')
     def application_hello():
-        from controllers import application_controller
+        from .controllers import application_controller
         return application_controller.hello()
 
-    from controllers import static_pages_controller
+    from .controllers import static_pages_controller
     app.register_blueprint(static_pages_controller.bp)
     return app
 
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
